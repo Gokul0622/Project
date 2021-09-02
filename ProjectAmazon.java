76d7a1e70faba33cbd4fdfee25cdce38ddc4c38a
Gokulraj Sugumar <88190729+Gokul0622@users.noreply.github.com>
2021-09-02 19:31:27 +0530

    Update ProjectAmazon.java

--- ProjectAmazon.java
+++ ProjectAmazon.java
@@ -1,5 +1,6 @@
-package week4.Day1.Assignments;
+package project;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -14,22 +15,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ProjectAmazon {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
+
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Thread.sleep(3000);
-
+		String parentWindowHandle=driver.getWindowHandle();
+		
 		WebElement SearchBar = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		SearchBar.sendKeys("oneplus 9 pro", Keys.ENTER);
 		Thread.sleep(3000);
-		driver.findElement(By.partialLinkText("OnePlus 9 Pro 5G ")).click();
-		Set<String> NewTab = driver.getWindowHandles();
-		WebElement mouseHover = driver.findElement(By.xpath("(//span[text()='4.2 out of 5 stars'])[1]"));
+		driver.findElement(By.partialLinkText("OnePlus 9 Pro 5G ")).click();	
+		
+		Set<String> WindowHandles = driver.getWindowHandles();
+		for (String NewWindows : WindowHandles) {
+			driver.switchTo().window(NewWindows);
+		
+		}
+		Thread.sleep(2000);
+		String Price = driver.findElement(By.xpath("//span[@id='priceblock_ourprice']")).getText();
+		System.out.println("Price of the First Displayed Phone is : " + Price );		
+		
+		String Ratings = driver.findElement(By.xpath("(//span[@id='acrCustomerReviewText'])[1]")).getText();
+		System.out.println("Price of the First Dsiplayed Phone is : " +Ratings );	
+	
+		WebElement mouseHover = driver.findElement(By.xpath("//div[@id='averageCustomerReviews_feature_div']//i[@class='a-icon a-icon-star a-star-4']"));
 		Actions action = new Actions(driver);
 		action.moveToElement(mouseHover).perform();
-
+		
 	}
+	
 }
